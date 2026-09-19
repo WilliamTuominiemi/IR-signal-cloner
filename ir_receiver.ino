a711b86c1c20403c193609c6c73c53b943e416bb
williamtuominiemi <william.m.tuominiemi@gmail.com>
2026-09-19 21:31:11 +0300

    first commit

---
+++ ir_receiver.ino
@@ -0,0 +1,17 @@
+#include <IRremote.hpp>
+#define IR_RECEIVE_PIN 7
+
+void setup()
+{
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+      IrReceiver.printIRResultShort(&Serial);
+      IrReceiver.printIRSendUsage(&Serial);
+      IrReceiver.resume();
+  }
+}
