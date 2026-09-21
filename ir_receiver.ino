74e52c62e5524057d09d130659e4c66a9a15e161
williamtuominiemi <william.m.tuominiemi@gmail.com>
2026-09-21 20:49:37 +0300

    button push sends IR signal

--- ir_receiver.ino
+++ ir_receiver.ino
@@ -1,17 +1,25 @@
 #include <IRremote.hpp>
 #define IR_RECEIVE_PIN 7
 #define IR_SEND_PIN 3
+#define BUTTON_PIN 2
+
+int buttonState = 0;
 
 void setup()
 {
+  pinMode(BUTTON_PIN, INPUT);
+
   Serial.begin(9600);
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
   IrSender.begin(IR_SEND_PIN);
 }
 
 void loop() {
-  IrSender.sendNEC(0x01, 0x02, 1); 
-  delay(1000); 
+  buttonState = digitalRead(BUTTON_PIN);
+
+  if (buttonState == HIGH) {
+    IrSender.sendNEC(0x01, 0x02, 1); 
+  }
 
   if (IrReceiver.decode()) {
       Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
